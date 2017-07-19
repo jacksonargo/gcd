89f89dc36438d75fa8d18cb06a026870dd2cd93d
Jackson Argo <jackson.argo@rackspace.com>
2017-07-18 23:29:12 -0500

    Added help for flask app

--- flask_gcd.py
+++ flask_gcd.py
@@ -3,6 +3,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/gcd')
+def flask_gcd_help():
+    return "Operations:\n\t/gcd/n1/n2\tReturns the GCD of n1 and n2."
+
 @app.route('/gcd/<int:n1>/<int:n2>')
 def flask_gcd(n1, n2):
     return gcd(n1, n2)
